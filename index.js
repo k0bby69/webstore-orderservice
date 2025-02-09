eeb3aaeea0952e92b0a2ea8960fa34ff88d090c7
k0bby69 <129067381+k0bby69@users.noreply.github.com>
2025-02-09 02:14:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const port = process.env.PORT || 8003
 
 app.use(express.json());
 app.use(cors());
+// app.use(cors({
+//   origin: ['http://localhost:3000'],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }))
 app.use(express.static(__dirname + "/public"));
 
 const { CreateChannel} = require("./utils");
