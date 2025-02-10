e8e39baecd68df63f5baf2599fc000be928e6a82
k0bby69 <129067381+k0bby69@users.noreply.github.com>
2025-02-10 03:40:50 +0000

    Update shopping-service.js

--- services/shopping-service.js
+++ services/shopping-service.js
@@ -1,8 +1,6 @@
 const ShoppingRepository = require("../database/repository/shopping-repository");
 const { FormatData } = require("../utils/");
-const Cart = require("../database/models/Cart");
-const Wishlist = require("../database/models/Wishlist");
-let print = console.log;
+const Cart = require('../database/models/Cart');
 class ShoppingService {
   constructor() {
     this.repository = new ShoppingRepository();
@@ -13,11 +11,9 @@ class ShoppingService {
     return FormatData(cartItems);
   }
 
-  async PlaceOrder(_id) {
-    const { orderResults, productDetails } =
-      await this.repository.CreateNewOrder(_id);
-
-    return FormatData({ orderResults, productDetails });
+  async PlaceOrder(orderData) {
+ 
+    return await this.repository.CreateNewOrder(orderData);
   }
 
   async GetOrders(customerId) {
@@ -31,6 +27,7 @@ class ShoppingService {
   }
 
   async ManageCart(customerId, item, amount, isRemove) {
+    
     const cartResult = await this.repository.AddCartItem(
       customerId,
       item,
@@ -40,132 +37,22 @@ class ShoppingService {
     return FormatData(cartResult);
   }
 
-  async EditWishlist(customerId, item, amount, isRemove) {
-    const wishlistResult = await this.repository.AddWishlistItem(
-      customerId,
-      item,
-      amount,
-      isRemove
-    );
-    return FormatData(wishlistResult);
-  }
-  async updateCart(productId, name, desc, img, type, stock, price, available) {
-    const carts = await Cart.find({ "items.product._id": productId });
-    print("carts", carts);
-    for (const cart of carts) {
-      cart.items = cart.items.map((item) => {
-        if (item.product._id == productId) {
-          print("ITEM HAS PRODUCT ID UPDATED??", item);
-          return {
-            ...item,
-            product: {
-              ...item.product,
-              name,
-              desc,
-              img,
-              stock,
-              price,
-              available,
-            },
-          };
- 
-        }
-        
-        return item
-      });
-      
-      await cart.save()
-    }
-  }
-
-  async updateWishlist(productId, name, desc, img, type, stock, price, available) {
-    const wishlists = await Wishlist.find({ "items.product._id": productId });
-    print("wishlists", wishlists);
-    for (const wishlist of wishlists) {
-      wishlist.items = wishlist.items.map((item) => {
-        if (item.product._id == productId) {
-          print("ITEM HAS PRODUCT ID UPDATED??", item);
-          return {
-            ...item,
-            product: {
-              ...item.product,
-              name,
-              desc,
-              img,
-              stock,
-              price,
-              available,
-            },
-          };
- 
-        }
-        
-        return item
-      });
-      
-      await wishlist.save()
-    }
-  }
-
-
-  async updateWishlist() {}
   async SubscribeEvents(payload) {
     payload = JSON.parse(payload);
-    console.log("INSIDE SHOPPPINGGG SERVICEEE");
     const { event, data } = payload;
-    console.log("EVENT X DATA CAUSING ERROR", event, data);
-    if (data !== undefined) {
-      const { userId, product, amount } = data;
-      console.log("SUBSCRIBE EVENTS FROM SHOPPING SERVICE", data);
-      //s
-      console.log("destructured data??????", userId, product, amount);
-      switch (event) {
-        case "ADD_TO_CART": {
-          const { userId, product, amount, isRemove } = data;
-          print("RECEIVED DATA ADD TO CART", userId, product, amount, isRemove);
-          this.ManageCart(userId, product, amount, isRemove);
-          break;
-        }
-        case "REMOVE_FROM_CART":
-          this.ManageCart(userId, product, amount, true);
-          break;
-        case "ADD_TO_WISHLIST":
-          this.EditWishlist(userId, product, amount, false);
-          break;
-        case "REMOVE_FROM_WISHLIST":
-          this.EditWishlist(userId, product, amount, true);
-          break;
-        case "UPDATE_CART_PRODUCT":
-          {
-            print("received data in update cart event", data);
-            await this.updateCart(
-              data.productId,
-              data.name,
-              data.desc,
-              data.img,
-              data.type,
-              data.stock,
-              data.price,
-              data.available
-            );
-            await this.updateWishlist(
-              data.productId,
-              data.name,
-              data.desc,
-              data.img,
-              data.type,
-              data.stock,
-              data.price,
-              data.available
-            );
-          }
-          break;
+    const { userId, product ,amount} = data;
+console.log('SUBSCRIBE EVENTS FROM SHOPPING SERVICE',data)
 
-        default:
-          console.log("the unavailable event is ", event);
-          console.log("this event is not valid");
-          break;
-      }
+    switch (event) {
+      case "ADD_TO_CART":
+        this.ManageCart(userId, product, amount, false);
+        break;
+      case "REMOVE_FROM_CART":
+        this.ManageCart(userId, product, amount, true);
+        break;
+      default:
+        console.log('this event is not valid')
+        break;
     }
   }
 
@@ -182,12 +69,16 @@ class ShoppingService {
     }
   }
   async GetProductPayload(productDetails, event) {
-    const payload = {
-      event: event,
-      data: productDetails,
-    };
 
-    return payload;
+      const payload = {
+        event: event,
+        data: 
+          productDetails
+        ,
+      };
+
+      return payload;
+    
   }
 
   async GetNotificationPayload(userEmail, order, event) {
@@ -202,6 +93,8 @@ class ShoppingService {
       return FormatData({ error: "No Order Available" });
     }
   }
+
+
 }
 
 module.exports = ShoppingService;
